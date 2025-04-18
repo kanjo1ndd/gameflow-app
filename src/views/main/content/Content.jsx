afa4d624fb8aa037dd174710dfcc7998ced4792f
kanjo1nd <dangbang568@gmail.com>
2025-04-18 20:20:11 +0300

    first block

--- src/views/main/content/Content.jsx
+++ src/views/main/content/Content.jsx
@@ -3,7 +3,17 @@ import './Content.css'
 export default function Content() {
     return <>
         <div className='content'>
-            <div className='first'></div>
+            <div className='first'>
+                <div className='first-img'></div>
+                <div className='first-div'>
+                    <div className='text-name'>NAME GAME</div>
+                    <div className='text-update'>NEW UPDATE</div>
+                    <div className='img'></div>
+                    <div className='img'></div>
+                    <div className='price'>FREE</div>
+                    <button>Learn more</button>
+                </div>
+            </div>
             <div className='content-div'></div>
             <div className='content-div'></div>
             <div className='content-div'></div>
