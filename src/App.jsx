1a76304b3fa6765f13e229f88a1a956739e13f6f
kanjo1nd <dangbang568@gmail.com>
2025-04-22 17:12:13 +0300

    registration

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Discover from './views/main/Discover'
 import SignIn from './views/registration/SignIn'
 import { Routes, Route } from 'react-router-dom'
+import SignUp from './views/registration/SignUp'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Discover />} />
         <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </>
   )
