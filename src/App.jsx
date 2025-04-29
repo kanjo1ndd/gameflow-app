2ece487854dc773d0c51fd977e5371f1ef9d5f56
kanjo1nd <dangbang568@gmail.com>
2025-04-30 02:54:22 +0300

    support

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from './views/registration/SignIn'
 import { Routes, Route } from 'react-router-dom'
 import SignUp from './views/registration/SignUp'
 import Profile from './views/profile/Profile'
+import Support from './views/support/Support'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Profile" element={<Profile />} />
+        <Route path="/Support" element={<Support />} />
       </Routes>
     </>
    )
