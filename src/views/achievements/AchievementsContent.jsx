a69fd19b39e40b8d4fc2efb02640174a29f7f34a
kanjo1nd <bezlimitnyj568@gmail.com>
2025-05-21 18:26:41 +0300

    achievements

--- src/views/achievements/AchievementsContent.jsx
+++ src/views/achievements/AchievementsContent.jsx
@@ -7,6 +7,10 @@ export default function AchievementsContent() {
             <div className='amount-Achievements'>Amount of achievements: ???</div>
             <div className='main-contentblock-for-Achievements'>
                 <Achievement />
+                <Achievement />
+                <Achievement />
+                <Achievement />
+                <Achievement />
             </div>
         </div>
     </>;
@@ -14,6 +18,15 @@ export default function AchievementsContent() {
 
 export function Achievement() {
     return <>
-        <div></div>
+        <div className='block-Achievement'>
+            <img className='img-Achievement'/>
+            <div className='information-block'>
+                <div className='name-description-Achievement'>
+                    <div className='name-Achievemnt'>Name Achievement</div>
+                    <div className='description-Achievemnt'>Description</div>
+                </div>
+                <div className='progress-Achievemnt'>??%</div>
+            </div>
+        </div>
     </>;
 }
